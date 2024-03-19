c2855b2af9d5e97f0fef362f636a03530b880097
Sumit <sumitkumar@kgpian.iitkgp.ac.in>
2024-03-19 15:06:49 +0530

    .

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 function App() {
   return (
     <>
+      <div className="App">
+        <header className="App-header">
+          <h1>OCMS</h1>
+          <p>
+            Open Course Management System
+          </p>
+          </header>
+        </div>
     </>
   );
 }
