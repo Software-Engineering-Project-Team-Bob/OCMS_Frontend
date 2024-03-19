42dbc3eab844ff0f8c40a542e3a5e1e8627a40d6
Sumit <sumitkumar@kgpian.iitkgp.ac.in>
2024-03-20 01:40:42 +0530

    adding the home page .js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
+import {BrowserRouter as Router, Route,Routes} from 'react-router-dom';
 import './App.css';
+import React, {useEffect} from 'react';
+import Home from './components/Home/Home';
+import ScrollToTop from './components/partials/ScrollToTop/ScrollToTop';
+import { useDispatch, useSelector } from 'react-redux';
+import { AUTOLOGIN, selectUserData } from './reduxSlices/authSlice';
 
-function App() {
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const App = () => {
+  const userData = useSelector(selectUserData);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(AUTOLOGIN());
+  }, []);
+
   return (
-    <>
-      <div className="App">
-        <header className="App-header">
-          <h1>OCMS</h1>
-          <p>
-            Open Course Management System
-          </p>
-          </header>
-        </div>
-    </>
-  );
-}
+    <div className="app">
+        <Router>
+      <ScrollToTop>
+          {
+            userData.loading ? (
+              <div className="col-12 d-flex justify-content-center align-items-center" style={{height:"100vh"}}>
+                <CircularProgress size={80} className="display-block"/>
+              </div>
+            ) : userData.token ? (
+              <Routes>
+                <Route path='/' element={<Home/>} exact/>
+                
 
-export default App;
+              </Routes> 
+            ) : (
+              <Routes>
+                <Route path='/' element={<Home/>} exact/>
+
+              </Routes>
+            )
+          }
+      </ScrollToTop>
+        </Router>
+    </div>
+  )
+} 
+
+export default App
