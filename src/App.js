dc12ce632e1d5eab844a777a4909e5cbf6575a21
Sumit <sumitkumar@kgpian.iitkgp.ac.in>
2024-03-22 01:06:28 +0530

    adding the classroom.css codes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {BrowserRouter as Router, Route,Routes} from 'react-router-dom';
 import './App.css';
 import React, {useEffect} from 'react';
 import Home from './components/Home/Home';
-// import Dashboard from './components/Dashboard/Dashboard';
+import Dashboard from './components/Dashboard/Dashboard';
 // import Classroom from './components/Classroom/Classroom';
 import ScrollToTop from './components/partials/ScrollToTop/ScrollToTop';
 // import AssignmentAdmin from './components/AssignmentAdmin/AssignmentAdmin';
@@ -32,12 +32,12 @@ const App = () => {
             ) : userData.token ? (
               <Routes>
                 <Route path='/' element={<Home/>} exact/>
-                {/* <Route path='/classes' element={<Dashboard/>} exact/>
-                <Route path='/classes/reminders' element={<Reminders/>}/>
-                <Route path='/classes/:id' element={<Classroom/>} exact/>
-                <Route path='/classes/:id/:tab' element={<Classroom/>} exact/>
-                <Route path='/classes/:id/assignment/:assignId/admin' element={<AssignmentAdmin/>} exact/>
-                <Route path='/classes/:id/assignment/:assignId' element={<StudentSubmission/>} exact/> */}
+                 <Route path='/classes' element={<Dashboard/>} exact/>
+                {/* <Route path='/classes/reminders' element={<Reminders/>}/> */}
+                {/* <Route path='/classes/:id' element={<Classroom/>} exact/> */}
+                {/* <Route path='/classes/:id/:tab' element={<Classroom/>} exact/> */}
+                {/* <Route path='/classes/:id/assignment/:assignId/admin' element={<AssignmentAdmin/>} exact/> */}
+                {/* <Route path='/classes/:id/assignment/:assignId' element={<StudentSubmission/>} exact/> */} 
 
               </Routes> 
             ) : (
