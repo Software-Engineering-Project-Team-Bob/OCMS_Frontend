539bce531b4890dd823c6b490204eaf8e6eec98b
Sumit <sumitkumar@kgpian.iitkgp.ac.in>
2024-03-22 13:53:21 +0530

    adding the assignment uploading eature

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Home from './components/Home/Home';
 import Dashboard from './components/Dashboard/Dashboard';
 import Classroom from './components/Classroom/Classroom';
 import ScrollToTop from './components/partials/ScrollToTop/ScrollToTop';
-
+import AssignmentAdmin from './components/AssignmentAdmin/AssignmentAdmin';
+import StudentSubmission from './components/StudentSubmission/StudentSubmission';
 import { useDispatch, useSelector } from 'react-redux';
 import { AUTOLOGIN, selectUserData } from './reduxSlices/authSlice';
 import Reminders from './components/partials/Header/MobileReminder'
@@ -31,11 +32,13 @@ const App = () => {
             ) : userData.token ? (
               <Routes>
                 <Route path='/' element={<Home/>} exact/>
-                 <Route path='/classes' element={<Dashboard/>} exact/>
+                <Route path='/classes' element={<Dashboard/>} exact/>
                 <Route path='/classes/reminders' element={<Reminders/>}/>
                 <Route path='/classes/:id' element={<Classroom/>} exact/>
                 <Route path='/classes/:id/:tab' element={<Classroom/>} exact/>
-                
+                <Route path='/classes/:id/assignment/:assignId/admin' element={<AssignmentAdmin/>} exact/>
+                <Route path='/classes/:id/assignment/:assignId' element={<StudentSubmission/>} exact/>
+
               </Routes> 
             ) : (
               <Routes>
