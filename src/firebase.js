9452753a24d6f5d3a59678cbce7fa2d8d3267c17
Sumit Kumar <sumitkumar@kgpian.iitkgp.ac.in>
2024-05-26 13:53:42 +0530

    vercel deployment bug

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
